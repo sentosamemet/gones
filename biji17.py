78f322fe4b3e2beba12200d37d9ce55f4ad46491
uriahgones <uriahgones@gmail.com>
2025-05-14 19:13:16 +0700

    Update biji17.py

--- biji17.py
+++ biji17.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://0238fc3249582cff57dd__cr.au:ff4062bfa3b7eb74@gw.dataimpulse.com:10017',
-        'https': 'https://0238fc3249582cff57dd__cr.au:ff4062bfa3b7eb74@gw.dataimpulse.com:10017',
+        'http': 'http://7f80327c578b86068874__cr.au:13e3de6667a042d5@gw.dataimpulse.com:10017',
+        'https': 'https://7f80327c578b86068874__cr.au:13e3de6667a042d5@gw.dataimpulse.com:10017',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
