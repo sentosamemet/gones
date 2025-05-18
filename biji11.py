7c176946b252b5725fc6880c13243f274ed7c8ec
uriahgones <uriahgones@gmail.com>
2025-05-18 22:02:52 +0700

    Update biji11.py

--- biji11.py
+++ biji11.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://39a9c6434705bac43c94__cr.br:07990267f4a1f5c4@gw.dataimpulse.com:10111',
-        'https': 'https://39a9c6434705bac43c94__cr.br:07990267f4a1f5c4@gw.dataimpulse.com:10111',
+        'http': 'http://bc29ed51a4090debc81d__cr.br:83cab51ab3cc7226@gw.dataimpulse.com:10111',
+        'https': 'https://bc29ed51a4090debc81d__cr.br:83cab51ab3cc7226@gw.dataimpulse.com:10111',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
