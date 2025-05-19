2a0b2f6e829b6bfb73765725f1d2eaaa97614916
uriahgones <uriahgones@gmail.com>
2025-05-19 16:30:00 +0700

    Update biji1.py

--- biji1.py
+++ biji1.py
@@ -18,7 +18,7 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/5b98c39a-328f-49d0-ae82-7785b46f75ac")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/6c322b8b-f804-40e2-8d9f-2dd916763c76")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
