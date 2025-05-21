0b62c9f57952cb9b3ab2cfb6bdfccb5eef347edd
uriahgones <uriahgones@gmail.com>
2025-05-21 19:58:19 +0700

    Update biji4.py

--- biji4.py
+++ biji4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://e1c4f1992994a9b8fa46__cr.bb:101b5d49ffd54635@gw.dataimpulse.com:10004',
-        'https': 'https://e1c4f1992994a9b8fa46__cr.bb:101b5d49ffd54635@gw.dataimpulse.com:10004',
+        'http': 'http://e1c4f1992994a9b8fa46__cr.bb:101b5d49ffd54635@gw.dataimpulse.com:10104',
+        'https': 'https://e1c4f1992994a9b8fa46__cr.bb:101b5d49ffd54635@gw.dataimpulse.com:10104',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
