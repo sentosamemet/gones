a988d3cff2d28caa8dd6e6dffdeaa41e0fef45ed
uriahgones <uriahgones@gmail.com>
2025-05-26 03:03:45 +0700

    Update biji15.py

--- biji15.py
+++ biji15.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://2db9c33c9c8a749b0b9a__cr.bg:eb1da026a5134dfa@gw.dataimpulse.com:10115',
-        'https': 'https://2db9c33c9c8a749b0b9a__cr.bg:eb1da026a5134dfa@gw.dataimpulse.com:10115',
+        'http': 'http://38b04dbf1a5e86316fb3__cr.bg:f69891643f6409af@gw.dataimpulse.com:10115',
+        'https': 'https://38b04dbf1a5e86316fb3__cr.bg:f69891643f6409af@gw.dataimpulse.com:10115',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
