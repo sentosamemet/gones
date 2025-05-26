35337a505bc9de9b36ce7c72789ec8b767da8673
uriahgones <uriahgones@gmail.com>
2025-05-27 00:03:56 +0700

    Update biji18.py

--- biji18.py
+++ biji18.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/bab95a52-67a1-4e3b-86b4-1dbe453cb357")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/400cedb2-9a49-45ec-b2a5-848c19bbf2e7")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
