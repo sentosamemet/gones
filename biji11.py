7148c6824d03672098bd34a8bb6bbe2713ed2648
uriahgones <uriahgones@gmail.com>
2025-05-27 04:47:08 +0700

    Update biji11.py

--- biji11.py
+++ biji11.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://38b04dbf1a5e86316fb3__cr.ve:f69891643f6409af@gw.dataimpulse.com:10011',
-        'https': 'https://38b04dbf1a5e86316fb3__cr.ve:f69891643f6409af@gw.dataimpulse.com:10011',
+        'http': 'http://7297dc87f43b3e403ee3__cr.ve:c84603fd3f59eb06@gw.dataimpulse.com:10011',
+        'https': 'https://7297dc87f43b3e403ee3__cr.ve:c84603fd3f59eb06@gw.dataimpulse.com:10011',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/12998c56-671e-456d-b76b-0e4f360f8d59")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/4ef63da7-834b-495e-b1f8-1cfb1b1d3fcf")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
