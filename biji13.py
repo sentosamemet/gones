f09d7b6d250efb3c4f15fd9a849d5af9ee17d991
uriahgones <uriahgones@gmail.com>
2025-05-28 05:36:14 +0700

    Update biji13.py

--- biji13.py
+++ biji13.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://7297dc87f43b3e403ee3__cr.cl:c84603fd3f59eb06@gw.dataimpulse.com:10113',
-        'https': 'https://7297dc87f43b3e403ee3__cr.cl:c84603fd3f59eb06@gw.dataimpulse.com:10113',
+        'http': 'http://a5b59f523705acd79f42__cr.cl:5ab0f5ec36c7f3f9@gw.dataimpulse.com:10113',
+        'https': 'https://a5b59f523705acd79f42__cr.cl:5ab0f5ec36c7f3f9@gw.dataimpulse.com:10113',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
