930d8011cd75e1ba4f7887e6df57ea450caa6d98
uriahgones <uriahgones@gmail.com>
2025-06-05 19:33:10 +0700

    Update biji14.py

--- biji14.py
+++ biji14.py
@@ -47,7 +47,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/e8b9b54e-a5f3-4665-b656-a775f923f9c4")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/2e63cb5b-b2ba-4ef8-8a15-a7076799390a")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
