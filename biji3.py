82869d825904b5c51609371de929f8921396bf93
uriahgones <uriahgones@gmail.com>
2025-06-07 03:50:52 +0700

    Update biji3.py

--- biji3.py
+++ biji3.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://1dbaa052cf65bb3db213__cr.au:6dc234a7f64de12d@gw.dataimpulse.com:10103',
-        'https': 'https://1dbaa052cf65bb3db213__cr.au:6dc234a7f64de12d@gw.dataimpulse.com:10103',
+        'http': 'http://79116e362a3f6b21cf7f__cr.au:6dcd35a068d29504@gw.dataimpulse.com:10103',
+        'https': 'https://79116e362a3f6b21cf7f__cr.au:6dcd35a068d29504@gw.dataimpulse.com:10103',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
