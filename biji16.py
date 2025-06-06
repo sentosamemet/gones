d16ef21260578ad5a27b0a54261ab0cadf96b7c3
uriahgones <uriahgones@gmail.com>
2025-06-07 04:17:37 +0700

    Update biji16.py

--- biji16.py
+++ biji16.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10116',
-        'https': 'https://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10116',
+        'http': 'http://79116e362a3f6b21cf7f__cr.br:6dcd35a068d29504@gw.dataimpulse.com:10116',
+        'https': 'https://79116e362a3f6b21cf7f__cr.br:6dcd35a068d29504@gw.dataimpulse.com:10116',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
